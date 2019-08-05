dc40f78fcd5db33fa29bba0c45d37f4c8dc6b4fe
bboyyu <bbo51@icloud.com>
2019-08-06 08:38:58 +0900

    bboyyu51 name

--- SamplePlugin/src/sample/Main.php
+++ SamplePlugin/src/sample/Main.php
@@ -1,5 +1,20 @@
 <?php
 
+/**
+ * _     _                             ____  _ 
+ *| |__ | |__   ___  _   _ _   _ _   _| ___|/ |
+ *| '_ \| '_ \ / _ \| | | | | | | | | |___ \| |
+ *| |_) | |_) | (_) | |_| | |_| | |_| |___) | |
+ *|_.__/|_.__/ \___/ \__, |\__, |\__,_|____/|_|
+ *                   |___/ |___/     
+ *           
+ * DiscordWebhook Sample Plugin
+ * 
+ * @see https://github.com/bboyyu51/DiscordWebhook
+ * @author bboyyu51 <bbo51@icloud.com>
+ * @copyright 2019 bboyyu51
+ */
+
 namespace sample;
 
 use pocketmine\plugin\PluginBase;
