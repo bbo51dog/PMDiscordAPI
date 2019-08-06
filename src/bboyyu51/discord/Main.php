2225fe17352c331fc473cb63f0c41c59b4562769
bboyyu51 <bbo51@icloud.com>
2019-08-07 00:26:46 +0900

    Create Main class

---
+++ src/bboyyu51/discord/Main.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * _     _                             ____  _ 
+ *| |__ | |__   ___  _   _ _   _ _   _| ___|/ |
+ *| '_ \| '_ \ / _ \| | | | | | | | | |___ \| |
+ *| |_) | |_) | (_) | |_| | |_| | |_| |___) | |
+ *|_.__/|_.__/ \___/ \__, |\__, |\__,_|____/|_|
+ *                   |___/ |___/
+ * Send Message to Discord Library
+ * 
+ * @see https://github.com/bboyyu51/DiscordWebhook
+ * @author bboyyu51 <bbo51@icloud.com>
+ * @copyright 2019 bboyyu51
+ */
+
+namespace bboyyu51\discord;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+
+}
