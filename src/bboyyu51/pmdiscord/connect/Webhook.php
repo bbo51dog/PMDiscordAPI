90ae4715c88c84ea2386ed579a1bc7879dbfee1a
bboyyu <bbo51@icloud.com>
2019-08-08 16:34:42 +0900

    edit Webhook

--- src/bboyyu51/pmdiscord/connect/Webhook.php
+++ src/bboyyu51/pmdiscord/connect/Webhook.php
@@ -1,15 +1,15 @@
 <?php
 
 /**
- * _     _                             ____  _ 
+ * _     _                             ____  _
  *| |__ | |__   ___  _   _ _   _ _   _| ___|/ |
  *| '_ \| '_ \ / _ \| | | | | | | | | |___ \| |
  *| |_) | |_) | (_) | |_| | |_| | |_| |___) | |
  *|_.__/|_.__/ \___/ \__, |\__, |\__,_|____/|_|
- *                   |___/ |___/     
- *           
+ *                   |___/ |___/
+ *
  * Send Message to Discord API
- * 
+ *
  * @license https://opensource.org/licenses/mit-license.html MIT License
  * @see https://github.com/bboyyu51/PM-DiscordAPI
  * @author bboyyu51 <bbo51@icloud.com>
@@ -18,6 +18,20 @@
 
 namespace bboyyu51\pmdiscord\connect;
 
+use bboyyu51\pmdiscord\content\ContentBase;
+
 class Webhook{
-    
+
+    /** @var string */
+    private $sender_name;
+
+    /** @var string */
+    private $webhook_url;
+
+    /** @var ContentBase[] */
+    private $content;
+
+    public function __construct(){
+
+    }
 }
