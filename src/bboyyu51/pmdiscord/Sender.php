9399c2e082cf7f5378dbe526471ea4b2dae93bce
bboyyu51 <bbo51@icloud.com>
2019-08-16 08:32:53 +0900

    Sender

--- src/bboyyu51/pmdiscord/Sender.php
+++ src/bboyyu51/pmdiscord/Sender.php
@@ -23,45 +23,29 @@ use bboyyu51\pmdiscord\connect\Webhook;
 
 class Sender{
     
-    /** @var string */
-    private $sender_name;
-    
-    /** @var string */
-    private $webhook_url;
-    
-    public function __construct(string $webhook_url){
-        $file = file_get_contents($webhook_url);
-        $json = json_decode($webhook_url, true);
-        $this->sender_name = $json["name"];
-        $this->webhook_url = $webhook_url;
-    }
-    
     /**
      * Send Message to Discord
      *
-     * @param string $message
+     * @param Webhook $webhook
      */
-    public function Send(string $message): void{
+    public static function Send(Webhook $webhook): void{
         $options = [
             'http' => [
                 'method' => 'POST',
                 'header' => 'Content-Type: application/json',
-                'content' => json_encode([
-                    'username' => $this->sender_name,
-                    'content' => $message,
-                ]),
+                'content' => json_encode($webhook->getData()),
             ],
         ];
-        $response = file_get_contents($this->webhook_url, false, stream_context_create($options));
+        file_get_contents($webhook->getUrl(), false, stream_context_create($options));
     }
     
     /**
      * Send Message by AsyncTask
      *
-     * @param string $message
+     * @param Webhook $webhook
      */
-    public function AsyncSend(string $message){
-        Server::getInstance()->getAsyncPool()->submitTask(new AsyncSendTask($message, $this));
+    public static function AsyncSend(Webhook $webhook): void{
+        Server::getInstance()->getAsyncPool()->submitTask(new AsyncSendTask($webhook));
     }
     
     /**
