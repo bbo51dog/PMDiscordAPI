2ec5e1f9fa9ea8858c3018acaffc287af0d62153
bboyyu51 <bbo51@icloud.com>
2019-08-16 20:51:34 +0900

    Create Embed class

---
+++ src/bboyyu51/pmdiscord/structure/Embed.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * _     _                             ____  _ 
+ *| |__ | |__   ___  _   _ _   _ _   _| ___|/ |
+ *| '_ \| '_ \ / _ \| | | | | | | | | |___ \| |
+ *| |_) | |_) | (_) | |_| | |_| | |_| |___) | |
+ *|_.__/|_.__/ \___/ \__, |\__, |\__,_|____/|_|
+ *                   |___/ |___/     
+ *           
+ * Send Message to Discord API
+ * 
+ * @license https://opensource.org/licenses/mit-license.html MIT License
+ * @see https://github.com/bboyyu51/PM-DiscordAPI
+ * @author bboyyu51 <bbo51@icloud.com>
+ * @copyright 2019 bboyyu51
+ */
+
+namespace bboyyu51\pmdiscord\structure;
+
+class Embed extends Structure{
+
+    /** @var string */
+    protected const TYPE = "embeds";
+    
+    public function get(){
+        return $this->data;
+    }
+
+    public function type(): string{
+        return self::TYPE;
+    }
+}
