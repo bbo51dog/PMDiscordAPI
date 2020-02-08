80bbfda74b8db8f5abed61d12e4d6a637b37db98
bbo51dog <bbo51@icloud.com>
2020-02-08 13:32:23 +0900

    Change namespace

--- src/bbo51dog/pmdiscord/Sender.php
+++ src/bbo51dog/pmdiscord/Sender.php
@@ -1,27 +1,11 @@
 <?php
 
-/**
- * _     _                             ____  _ 
- *| |__ | |__   ___  _   _ _   _ _   _| ___|/ |
- *| '_ \| '_ \ / _ \| | | | | | | | | |___ \| |
- *| |_) | |_) | (_) | |_| | |_| | |_| |___) | |
- *|_.__/|_.__/ \___/ \__, |\__, |\__,_|____/|_|
- *                   |___/ |___/     
- *           
- * Send Message to Discord API
- * 
- * @license https://opensource.org/licenses/mit-license.html MIT License
- * @see https://github.com/bbo51dog/PM-DiscordAPI
- * @author bbo51dog <bbo51@icloud.com>
- * @copyright 2019 bbo51dog
- */
-
 declare(strict_types = 1);
 
-namespace bboyyu51\pmdiscord;
+namespace bbo51dog\pmdiscord;
 
 use pocketmine\Server;
-use bboyyu51\pmdiscord\connect\Webhook;
+use bbo51dog\pmdiscord\connect\Webhook;
 
 class Sender{
     
---
+++ src/bbo51dog/pmdiscord/structure/Structure.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace bbo51dog\pmdiscord\structure;
+
+abstract class Structure{
+    
+    /** @var string */
+    protected const TYPE = "base";
+    
+    /** @var mixed */
+    protected $data;
+    
+    /** @return mixed */
+    public function get(){
+        return $this->data;
+    }
+    
+    abstract public function type(): string;
+}
